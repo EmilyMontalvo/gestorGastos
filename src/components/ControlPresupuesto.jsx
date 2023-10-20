0aeda627d2789166129247612e69d8fc8fdab05f
Emily Montalvo <emily.montalvo@procredit-group.com>
2023-10-20 14:19:35 -0500

    Cálculos v1

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
 
-const ControlPresupuesto = ({presupuesto}) => {
+const ControlPresupuesto = ({presupuesto, gastos}) => {
+
+    const [disponible, setDisponible] = useState(0)
+    const [gastado, setGastado] = useState(0)
+
+    useEffect(()=>{
+        
+    },[gastos])
   
     const formatearCantidad = cantidad => {
         return cantidad.toLocaleString("en-US",{
-            style: "currency",
+            style: "currency", 
             currency: "USD"
         })
     }
@@ -19,8 +26,8 @@ const ControlPresupuesto = ({presupuesto}) => {
         </div>
         <div className="contenido-presupuesto">
             <p> <span>Presupuesto:</span> {formatearCantidad(presupuesto)} </p>
-            <p> <span>Disponible:</span> {formatearCantidad(0)} </p>
-            <p> <span>Gastado:</span> {formatearCantidad(0)} </p>
+            <p> <span>Disponible:</span> {formatearCantidad(disponible)} </p>
+            <p> <span>Gastado:</span> {formatearCantidad(gastado)} </p>
         </div>
       
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,14 @@ import NuevoPresupuesto from './NuevoPresupuesto'
 import ControlPresupuesto from './ControlPresupuesto'
 
 
-const Header = ({presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto}) => {
+const Header = ({presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto, gastos}) => {
   return (
     <header>
         <h1>Planificador de Gastos</h1>
         {isValidPresupuesto?(
             <ControlPresupuesto
             presupuesto = {presupuesto}
+            gastos ={gastos}
             />
         ):(
         <NuevoPresupuesto
