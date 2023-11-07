d955dfbd36a1a97416f237f2136c62ddfe072ed6
EmilyMontalvo <emily.montalvo3006@gmail.com>
2023-11-06 23:47:29 -0500

    Add Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import ListadoGastos from "./components/ListadoGastos"
 
 function App() {
 
-  const [presupuesto, setPresupuesto] = useState(0)
+  const [presupuesto, setPresupuesto] = useState(Number(localStorage.getItem("presupuesto")??0))
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
   const [modal, setModal] = useState(false)
   const [animarModal, setAnimarModal] = useState(false)
-  const [gastos, setGastos] = useState([])
+  const [gastos, setGastos] = useState(localStorage.getItem("gastos") ? JSON.parse(localStorage.getItem("gastos")):[])
   const [gastoEditar, setGastoEditar] = useState({})
 
   useEffect(()=>{
@@ -23,6 +23,20 @@ function App() {
 
   }, [gastoEditar])
 
+  useEffect(()=>{
+    localStorage.setItem("presupuesto", presupuesto?? 0)
+  },[presupuesto])
+
+  useEffect(()=>{
+    localStorage.setItem("gastos", JSON.stringify(gastos)?? [])
+  },[gastos])
+
+  useEffect(()=>{
+   const presupuestoLS = Number(localStorage.getItem("presupuesto"))??0
+   if(presupuestoLS>0){
+    setIsValidPresupuesto(true)
+   }
+  },[])
 
   const handleNuevoGasto = () => {
     setModal(true)
